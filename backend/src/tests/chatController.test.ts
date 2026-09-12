426e72df04a08f95e0cc50420eb3c90381e1514a
Gabs-X <ga13121110@gmail.com>
2026-09-12 16:52:19 -0300

    feat: create tests

---
+++ backend/src/tests/chatController.test.ts
@@ -0,0 +1,65 @@
+import type { Request, Response } from "express";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { sendMessage } from "../controllers/chatController";
+import { askGemini } from "../services/geminiService";
+
+vi.mock("../services/geminiService", () => ({
+    askGemini: vi.fn(),
+}));
+
+describe("chatController", () => {
+    const mockedAskGemini = vi.mocked(askGemini);
+
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it("returns 40000 when is empty", async () => {
+        const request = {
+            body: {
+                message: "",
+            }
+        } as Request;
+        
+        const json = vi.fn();
+
+        const response = {
+            status: vi.fn().mockReturnValue({ json }),
+            json,
+        } as unknown as Response;
+
+        await sendMessage(request, response);
+
+        expect(response.status).toHaveBeenCalledWith(400);
+
+        expect(json).toHaveBeenCalledWith({ error: "Message is required" });
+
+        expect(mockedAskGemini).not.toHaveBeenCalled();
+    });
+
+    it("returns Gemini response when message is valid", async () => {
+        mockedAskGemini.mockResolvedValue("Hello! How can I help you today?");
+
+        const request = {
+            body: {
+                message: "Hello",
+            },
+        } as Request;
+
+        const json = vi.fn();
+
+        const response = {
+            status: vi.fn(),
+            json,
+        } as unknown as Response;
+
+        await sendMessage(request, response);
+
+        expect(mockedAskGemini).toHaveBeenCalledWith("Hello");
+
+        expect(json).toHaveBeenCalledWith({ response: "Hello! How can I help you today?" });
+
+    });
+
+});
